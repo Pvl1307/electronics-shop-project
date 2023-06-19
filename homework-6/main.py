6b085522522fe4d8c97584b65a58bad87868de78
Pvl <ivanov.pavel.1307@gmail.com>
2023-06-19 23:25:27 +0200

    HM6

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,14 @@
-from src.item import Item
+from src.item import Item, InstantiateCSVError
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    try:
+        Item.instantiate_from_csv()
     # FileNotFoundError: Отсутствует файл item.csv
-
+    except FileNotFoundError as e:
+        print(e)
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+
+    except InstantiateCSVError as e:
+        print(e.message)
     # InstantiateCSVError: Файл item.csv поврежден
